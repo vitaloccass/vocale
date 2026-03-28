70a3d13e5b0ec51a9e53fef22d252e471d602162
Mahefa <todisoa.rakotoarijaona@arbiochem.mg>
2026-03-28 05:37:43 +0300

    Modif Mahefa 20260328 Matin -4

--- static/app.js
+++ static/app.js
@@ -2622,9 +2622,9 @@ function calculerTTC(){
             ];
 
             if (fournisseursSpeciaux.includes(code_fournisseur)) {
-                mtt = Number(qte) * ((Number(pu) * 1.2)* (1 - Number(remise) / 100));
+                mtt = (Number(qte) * ((Number(pu) * (1 - Number(remise) / 100)))) * 1.2;
             } else {
-                mtt = Number(qte) * Number(pu) * (1 - Number(remise) / 100);
+                mtt = Number(qte) * (Number(pu) * (1 - Number(remise) / 100));
             }
 
             tds[11].innerText = mtt.toFixed(2);

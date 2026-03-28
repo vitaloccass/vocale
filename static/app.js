69b3a2943b3c1b4d133315f03981d7e5f954a736
Mahefa <todisoa.rakotoarijaona@arbiochem.mg>
2026-03-28 05:16:37 +0300

    Modif Mahefa 20260328 Matin -1

--- static/app.js
+++ static/app.js
@@ -2278,7 +2278,7 @@ function traiterCommande(transcript) {
         if (motCle) {
             if(motCle=='repas'){
                 filtrerArticles(['vary','henomby','pizza','mayonnaise','ketchup','pate','saucisse','totokena','vantan']);
-            }else if(motCle=='boisson' || motCle=='boissons'){
+            }else if(motCle=='boisson' || motCle=='boissons' || motCle=='Boissons'){
                 filtrerArticles(['JUS NATUREL','CHOCOLAT CHAUD','THE A LA MENTHE MAISON','LAIT','THE GLACE MAISON','CAFE','THE NOIR','ORANGE GAZEUSE','EAU DE MONTAGNE 1L']);
             }else{
                 filtrerArticles(motCle);

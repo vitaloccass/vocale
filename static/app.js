ca888a73e845f2bfd8b245e38c1d0d6920aa0f4d
Mahefa <todisoa.rakotoarijaona@arbiochem.mg>
2026-06-30 09:04:49 +0300

    Modif Mahefas 20260630 Matinsssf11

--- static/app.js
+++ static/app.js
@@ -2978,9 +2978,9 @@ function exporterTXT() {
     const content = lines.join("\r\n") + "\r\n";
     const tsenaFinal = (recup_tsena || "").replace('LOCCA ', '');
     const now = new Date();
-    let heure = String(now.getHours()).padStart(2, '0');
-    let minute = String(now.getMinutes()).padStart(2, '0');
-    let seconde = String(now.getSeconds()).padStart(2, '0');
+    const heure = String(now.getHours()).padStart(2, '0');
+    const minute = String(now.getMinutes()).padStart(2, '0');
+    const seconde = String(now.getSeconds()).padStart(2, '0');
 
     // 📁 Nom du fichier
     let filename = '';
